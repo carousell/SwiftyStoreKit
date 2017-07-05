cdd3eb8dc22eb4ac1da01f9c3ddb4910babbaaba
Luong Huy Duc <luonghuyduc88@gmail.com>
2017-07-05 17:18:41 +0800

    Expose access to unhandledTransactions

--- SwiftyStoreKit/SwiftyStoreKit.swift
+++ SwiftyStoreKit/SwiftyStoreKit.swift
@@ -130,6 +130,10 @@ extension SwiftyStoreKit {
     fileprivate static let sharedInstance = SwiftyStoreKit()
 
     // MARK: Public methods - Purchases
+
+	public class var unhandledTransactions: [Purchase] {
+		return sharedInstance.paymentQueueController.unhandledTransactions
+	}
     
     /**
      * Return NO if this device is not able or allowed to make payments
